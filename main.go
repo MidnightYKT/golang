5a9e12a69174121851ca1f9692bb83083936ebe0
MidnightYKT <nomiama_ykt@mail.ru>
2021-10-16 13:36:26 +0900

    Ex1

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	name string
+	age  uint16
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	s.ListenAndServe()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(isPalindrome("121"))
+}
+
+func isPalindrome(input string) bool {
+	for i := 0; i < len(input)/2; i++ {
+		if input[i] != input[len(input)-i-1] {
+			return false
+		}
+	}
+	return true
 }
